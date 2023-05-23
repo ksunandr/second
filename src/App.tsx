d71685a4d0d9a1521ff6a1ccf59fa52f414183b5
ksan <kseniia.andreeva@diffblue.com>
2023-05-23 21:46:50 +0100

    fech data

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
-import React, { useState } from 'react'
-import {Product} from './components/Product'
-import { products } from './components/data/products';
+import React, { useEffect, useState } from 'react'
+import { Product } from './components/Product'
+// import { products } from './components/data/products';
+import axios from 'axios';
+import { IProduct } from './models';
 
 
 function App() {
+
+  const [products, setProducts] = useState<IProduct[]>([])
+  async function fetchProducts() {
+    const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=6')
+    setProducts((response).data)
+    console.log(response)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+    console.log("useEffect")
+  }, [])
+
   return (
-  <div className="container mx-auto max-w-2xl pt-5">
-
-<Product product = {products[0]}  />
-<Product product = {products[1]}  />
-
-  </div>
+    <div className="container mx-auto max-w-2xl pt-5">
+      {products.map(p => <Product product={p} key={p.id} />)}
+      {/* {products.map((p, index) => <Product product = {p} key={index} />)} */}
+    </div>
   )
-}   
+}
 
 export default App;
