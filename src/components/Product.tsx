1e168ab1d60b10ba99e553973546785afaca8ad3
ksan <kseniia.andreeva@diffblue.com>
2023-05-23 21:24:18 +0100

    455

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -7,7 +7,9 @@ interface ProductProps {
 
 export function Product({ product }: ProductProps) {
 
-    const[details, setDetails] = useState(false);
+    const [details, setDetails] = useState(false)
+    const btnBgClassName = details ? 'bg-yellow-400' : 'bg-blue-400'
+    const btnClasses =['py-2 px-4 border', btnBgClassName]
 
     return (
         <div className="border py-2 px-4 rounded flex flex-col items-center mb-2"
@@ -17,12 +19,15 @@ export function Product({ product }: ProductProps) {
                 alt={product.title} />
             <p>{product.title}</p>
             <span className="font-bold">{product.price}</span>
-            <button className="py-2 px-4 border bg-yellow-400"
-            onClick={() => setDetails(!details)}
-            >Show details</button>
-            {details && <div>
+            <button className = {btnClasses.join(' ')}
+                onClick={() => setDetails(prev => !prev)}
+            >{details ? 'Hide details': 'Show details'}</button>
+            {details && 
+            <div>
                 <p>{product.description}</p>
-            </div>}
+                <p>Rate: <span style={{fontWeight: 'bold'}}>{product.rating.rate}</span></p>
+            </div>
+            }
         </div>
     )
 }
