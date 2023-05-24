ffbd6b15fc63f06027f4792796384e7a4e998e32
ksan <kseniia.andreeva@diffblue.com>
2023-05-24 13:53:04 +0100

    create new product post call

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { CreateProduct } from './components/CreateProduct';
 import { ErorrText } from './components/ErorrText';
 import { Loader } from './components/Loader';
+import { Modal } from './components/Modal';
 import { Product } from './components/Product'
 import { useProducts } from './hooks/products';
 
@@ -10,10 +12,15 @@ function App() {
   return (
     <div className="container mx-auto max-w-2xl pt-5">
       {loading && <Loader />}
-      {error && <ErorrText error = {error}/>}
+      {error && <ErorrText error={error} />}
 
       {products.map(p => <Product product={p} key={p.id} />)}
       {/* {products.map((p, index) => <Product product = {p} key={index} />)} */} {/* another way to add key - using index*/}
+
+      <Modal title="Create new product">
+        <CreateProduct />
+
+      </Modal>
     </div>
   )
 }
